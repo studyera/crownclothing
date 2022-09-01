c17f660801d3a393becfcd9b188dc62a2e732855
Mohit yadav <mohyadav3@publicisgroupe.net>
2022-09-01 09:24:24 +0530

    final site

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -4,23 +4,9 @@ import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
 import SignInForm from "../../components/sign-in-form/sign-in-form.component";
 import "./authentication.styles.scss"
 const Authentication = () => {
-//   useEffect(async () => {
-//     const response = await getRedirectResult(auth);
-//     console.log(response);
-
-//     if (response) {
-//       const userDocRef = await createUserDocumentFromAuth(response.user);
-//     }
-//   }, []);
-
-  
 
   return (
     <div className="authentication-container">
-      {/* <button onClick={logGoogleUser}>sign in with google</button> */}
-      {/* <button onClick={signInWithGoogleRedirect}>
-        sign in with google redirect
-      </button> */}
       <SignInForm />
       <SignUpForm />
     </div>
